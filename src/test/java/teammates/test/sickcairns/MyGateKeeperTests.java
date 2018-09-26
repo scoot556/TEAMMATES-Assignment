700457fecf02e3ee84f0dbf3dfbb948df8823f46
s3516805 <s3516805@student.rmit.edu.au>
2018-09-26 13:48:21 +1000

    Case sensitive test cases for Gatekeeper added

--- src/test/java/teammates/test/sickcairns/MyGateKeeperTests.java
+++ src/test/java/teammates/test/sickcairns/MyGateKeeperTests.java
@@ -56,5 +56,57 @@ public class MyGateKeeperTests extends GateKeeperTest {
     	}
     	assertFalse(isRMIT);
     }
+    
+	@Test
+    public void test_caseSensitiveValidRMITEmail() throws Exception{
+    	boolean isRMIT = false;
+    	gaeSimulation.loginUser("s3516805@sTuDenT.RmiT.eDu.Au");
+    	String user = gateKeeper.getCurrentUser().id;
+    	
+    	if(gateKeeper.isRMIT(user)) {
+    		isRMIT = true;
+    	}
+    	
+    	assertTrue(isRMIT);
+    }
+	
+	@Test
+    public void test_caseSensitiveValidRMITEmail_2() throws Exception{
+    	boolean isRMIT = false;
+    	gaeSimulation.loginUser("s3537805@STuDEnT.RMiT.EDu.Au");
+    	String user = gateKeeper.getCurrentUser().id;
+    	
+    	if(gateKeeper.isRMIT(user)) {
+    		isRMIT = true;
+    	}
+    	
+    	assertTrue(isRMIT);
+    }
+	
+	@Test(expectedExceptions = UnauthorizedAccessException.class)
+    public void test_invalidCaseSensitiveValidRMITEmail() throws Exception{
+    	boolean isRMIT = false;
+    	gaeSimulation.loginUser("s3516805@sTuDen.RmiT.eDu.Au");
+    	String user = gateKeeper.getCurrentUser().id;
+    	
+    	if(gateKeeper.isRMIT(user)) {
+    		isRMIT = true;
+    	}
+    	
+    	assertFalse(isRMIT);
+    }
+	
+	@Test(expectedExceptions = UnauthorizedAccessException.class)
+    public void test_invalidCaseSensitiveValidRMITEmail_2() throws Exception{
+    	boolean isRMIT = false;
+    	gaeSimulation.loginUser("s3537805@sTUDen.RmiT.eDu.Au");
+    	String user = gateKeeper.getCurrentUser().id;
+    	
+    	if(gateKeeper.isRMIT(user)) {
+    		isRMIT = true;
+    	}
+    	
+    	assertFalse(isRMIT);
+    }
 	
 }
