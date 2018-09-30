e089c08281f3053295248bf7cf054298461d4338
Joshua Orozco <s3485376@student.rmit.edu.au>
2018-09-30 14:37:11 +1000

    Rename to lowercase variables

--- src/test/java/teammates/test/cases/logic/GateKeeperTest.java
+++ src/test/java/teammates/test/cases/logic/GateKeeperTest.java
@@ -73,52 +73,52 @@ public class GateKeeperTest extends BaseLogicTest {
     }
     
     @Test
-    public void test_validRmitEmail() throws Exception{
-    	boolean isRMIT = false;
-    	gaeSimulation.loginUser("s3516805@student.rmit.edu.au");
-    	String user = gateKeeper.getCurrentUser().id;
-    	
-    	if(gateKeeper.isRmit(user)) {
-    		isRMIT = true;
-    	}
-    	
-    	assertTrue(isRMIT);
+    public void test_validRmitEmail() throws Exception {
+        boolean isRmit = false;
+        gaeSimulation.loginUser("s3516805@student.rmit.edu.au");
+        String user = gateKeeper.getCurrentUser().id;
+        
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
+        }
+        
+        assertTrue(isRmit);
     }
     
     @Test
-    public void test_validRmitEmail_2() throws Exception{
-    	boolean isRMIT = false;
-    	gaeSimulation.loginUser("s3516805@rmit.edu.au");
-    	String user = gateKeeper.getCurrentUser().id;
-    	
-    	if(gateKeeper.isRmit(user)) {
-    		isRMIT = true;
-    	}
-    	
-    	assertTrue(isRMIT);
+    public void test_validRmitEmail_2() throws Exception {
+        boolean isRmit = false;
+        gaeSimulation.loginUser("s3516805@rmit.edu.au");
+        String user = gateKeeper.getCurrentUser().id;
+        
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
+        }
+        
+        assertTrue(isRmit);
     }
 
     @Test(expectedExceptions = UnauthorizedAccessException.class)
-    public void test_invalidRmitEmail() throws Exception{
-    	boolean isRMIT = false;
-    	gaeSimulation.loginUser("abcde@student.rmit.edu");
-    	String user = gateKeeper.getCurrentUser().id;
+    public void test_invalidRmitEmail() throws Exception {
+        boolean isRmit = false;
+        gaeSimulation.loginUser("abcde@student.rmit.edu");
+        String user = gateKeeper.getCurrentUser().id;
 
-    	if(gateKeeper.isRmit(user)) {
-    		isRMIT = true;
-    	}
-    	assertFalse(isRMIT);
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
+        }
+        assertFalse(isRmit);
     }
     
     @Test(expectedExceptions = UnauthorizedAccessException.class)
-    public void test_invalidRmitEmail_2() throws Exception{
-    	boolean isRMIT = false;
-    	gaeSimulation.loginUser("sampleEmail@gmail.com");
-    	String user = gateKeeper.getCurrentUser().id;
+    public void test_invalidRmitEmail_2() throws Exception {
+        boolean isRmit = false;
+        gaeSimulation.loginUser("sampleEmail@gmail.com");
+        String user = gateKeeper.getCurrentUser().id;
 
-    	if(gateKeeper.isRmit(user)) {
-    		isRMIT = true;
-    	}
-    	assertFalse(isRMIT);
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
+        }
+        assertFalse(isRmit);
     }
 }
--- src/test/java/teammates/test/sickcairns/MyGateKeeperTests.java
+++ src/test/java/teammates/test/sickcairns/MyGateKeeperTests.java
@@ -8,53 +8,53 @@ import teammates.test.cases.logic.GateKeeperTest;
 public class MyGateKeeperTests extends GateKeeperTest {
 
     @Test
-    public void test_validRmitEmail() throws Exception{
-        boolean isRMIT = false;
+    public void test_validRmitEmail() throws Exception {
+        boolean isRmit = false;
         gaeSimulation.loginUser("s3516805@student.rmit.edu.au");
         String user = gateKeeper.getCurrentUser().id;
         
-        if(gateKeeper.isRmit(user)) {
-            isRMIT = true;
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
         }
         
-        assertTrue(isRMIT);
+        assertTrue(isRmit);
     }
     
     @Test
-    public void test_validRmitEmail_2() throws Exception{
-        boolean isRMIT = false;
+    public void test_validRmitEmail_2() throws Exception {
+        boolean isRmit = false;
         gaeSimulation.loginUser("s3516805@rmit.edu.au");
         String user = gateKeeper.getCurrentUser().id;
         
-        if(gateKeeper.isRmit(user)) {
-            isRMIT = true;
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
         }
         
-        assertTrue(isRMIT);
+        assertTrue(isRmit);
     }
 
     @Test(expectedExceptions = UnauthorizedAccessException.class)
-    public void test_invalidRmitEmail() throws Exception{
-        boolean isRMIT = false;
+    public void test_invalidRmitEmail() throws Exception {
+        boolean isRmit = false;
         gaeSimulation.loginUser("abcde@student.rmit.edu");
         String user = gateKeeper.getCurrentUser().id;
 
-        if(gateKeeper.isRmit(user)) {
-            isRMIT = true;
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
         }
-        assertFalse(isRMIT);
+        assertFalse(isRmit);
     }
     
     @Test(expectedExceptions = UnauthorizedAccessException.class)
-    public void test_invalidRmitEmail_2() throws Exception{
-        boolean isRMIT = false;
+    public void test_invalidRmitEmail_2() throws Exception {
+        boolean isRmit = false;
         gaeSimulation.loginUser("sampleEmail@gmail.com");
         String user = gateKeeper.getCurrentUser().id;
 
-        if(gateKeeper.isRmit(user)) {
-            isRMIT = true;
+        if (gateKeeper.isRmit(user)) {
+            isRmit = true;
         }
-        assertFalse(isRMIT);
+        assertFalse(isRmit);
     }
     
 }
